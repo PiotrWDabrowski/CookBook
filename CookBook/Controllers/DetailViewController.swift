bd6c6957d0baaf006e75898165289b9f66fa94c8
Piotr Dąbrowski <dabrowski.p.w@gmail.com>
2016-10-27 20:33:30 +0200

    SplitViewController set as core storyboard flow

---
+++ CookBook/Controllers/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  CookBook
+//
+//  Created by Piotr Dąbrowski on 27/10/16.
+//  Copyright © 2016 Piotr Dąbrowski. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+}
---
+++ CookBook/Controllers/MasterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.swift
+//  CookBook
+//
+//  Created by Piotr Dąbrowski on 27/10/16.
+//  Copyright © 2016 Piotr Dąbrowski. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+}
