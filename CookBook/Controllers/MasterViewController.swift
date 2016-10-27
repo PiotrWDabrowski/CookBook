151a31046275bcc6ff633a36ab4d9955cfd3cb74
Piotr Dąbrowski <dabrowski.p.w@gmail.com>
2016-10-27 23:43:24 +0200

    LightGray color treated as image placeholder.

--- CookBook/Controllers/MasterViewController.swift
+++ CookBook/Controllers/MasterViewController.swift
@@ -77,6 +77,7 @@ class MasterViewController: UITableViewController {
         cell.recipeTextLabel!.text = recipe.title
         cell.recipeDetailedTextLabel!.text = recipe.detailedDescription
         cell.recipeImage.image = nil
+        cell.recipeImage.backgroundColor = UIColor.lightGrayColor()
         
         return cell
     }
